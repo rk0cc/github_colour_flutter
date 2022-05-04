70b2d42c31e5c134179dfcf465f2c69f14cf9dba
Cyrus Chan <cyruschan1212@gmail.com>
2022-05-04 19:49:41 +0800

    Doc lib

--- lib/github_colour.dart
+++ lib/github_colour.dart
@@ -1,3 +1,6 @@
+/// Receive latest commit of
+/// [ozh's github-colors](https://github.com/ozh/github-colors) to Flutter's
+/// [Color].
 library github_colour;
 
 import 'dart:convert';
@@ -6,7 +9,9 @@ import 'package:flutter/widgets.dart' show Color;
 import 'package:http/http.dart' as http show get;
 import 'package:meta/meta.dart' show sealed;
 
+/// An [Error] thrown when response unsuccessfully.
 class GitHubColourLoadFailedError extends Error {
+  /// HTTP response code when fetching colour data.
   final int responseCode;
 
   GitHubColourLoadFailedError._(this.responseCode)
@@ -17,17 +22,25 @@ class GitHubColourLoadFailedError extends Error {
       "GitHubColourLoadFailedError: Can not receive GitHub language colour from server with HTTP code $responseCode.";
 }
 
+/// A class for getting GitHub language colour.
 @sealed
 class GitHubColour {
   static GitHubColour? _instance;
   final Map<String, Color> _githubLangColour;
 
+  /// A [String] of hex value when the language is undefined or null.
   static const String defaultColourHex = "#8f8f8f";
-  static const Color defaultColor = Color(0xff8f8f8f);
+
+  /// [Color] object of [defaultColourHex].
+  static const Color defaultColour = Color(0xff8f8f8f);
 
   GitHubColour._(Map<String, Color> githubLangColour)
       : this._githubLangColour = Map.unmodifiable(githubLangColour);
 
+  /// Construct an instance of [GitHubColour].
+  ///
+  /// If no instance created, it will construct and will be reused when
+  /// [getInstance] or [getExistedInstance] called again.
   static Future<GitHubColour> getInstance() async {
     if (_instance == null) {
       final Uri ghc = Uri.parse(
@@ -51,6 +64,9 @@ class GitHubColour {
     return _instance!;
   }
 
+  /// Get constructed instance which called [getInstance] early.
+  ///
+  /// It throws [UnimplementedError] if called with no existed instance.
   static GitHubColour getExistedInstance() {
     if (_instance == null) {
       throw UnimplementedError("No existed instance found in GitHubColour");
@@ -59,8 +75,14 @@ class GitHubColour {
     return _instance!;
   }
 
-  Color find(String language, {Color fallback = defaultColor}) =>
+  /// Find [Color] for the [language].
+  ///
+  /// If [language] is undefined or defined with `null`, it uses [fallback]
+  /// insteaded.
+  Color find(String language, {Color fallback = defaultColour}) =>
       _githubLangColour[language] ?? fallback;
 }
 
+/// Alias type for repersenting "colour" in American English which does exactly
+/// same with [GitHubColour].
 typedef GitHubColor = GitHubColour;
