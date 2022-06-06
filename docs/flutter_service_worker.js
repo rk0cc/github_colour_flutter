6fbe0d2a59ba100396315b9cc57b104973b8cd90
Cyrus Chan <cyruschan1212@gmail.com>
2022-06-06 12:43:40 +0800

    Use gh-page branches compiled one

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -22,9 +22,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b22c804874347d4c05394ace37440a0b",
-"/": "b22c804874347d4c05394ace37440a0b",
-"main.dart.js": "b3fbb7b8a120322bbc2370daf744a75a",
+"index.html": "b69b0a72b2fd0500deab52e2aabcc4b8",
+"/": "b69b0a72b2fd0500deab52e2aabcc4b8",
+"main.dart.js": "244123d3df95c6bb8c81cc2494b63881",
 "manifest.json": "0867c3e13649ac4d06fe34b7b3ddce08",
 "version.json": "4aca90c16e552baac0ccb44601ec5866"
 };
